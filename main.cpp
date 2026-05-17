7c72ae961e6a5bcdf874654cfb798ba50ab14ef8
SenPaiOfUrSenSei <aryanagarwal0876@gmail.com>
2026-05-18 01:33:54 +0530

    feat: add global state and configuration using event bus

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <hyprland/src/plugins/PluginAPI.hpp>
 #include <hyprland/src/Compositor.hpp>
+#include <hyprland/src/event/EventBus.hpp>
 
 inline HANDLE PHANDLE = nullptr;
+
+int g_iCurrentContext = 0;
+int g_iOffsetMultiplier = 100;
+
+void onConfigReload() {
+    g_iOffsetMultiplier = std::any_cast<Hyprlang::INT>(HyprlandAPI::getConfigValue(PHANDLE, "plugin:hyprcontexts:offset")->getValue());
+}
 
 APICALL EXPORT std::string PLUGIN_API_VERSION() {
     return HYPRLAND_API_VERSION;
@@ -9,6 +17,13 @@ APICALL EXPORT std::string PLUGIN_API_VERSION() {
 
 APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
     PHANDLE = handle;
+
+    HyprlandAPI::addConfigValue(PHANDLE, "plugin:hyprcontexts:offset", Hyprlang::INT{100});
+    HyprlandAPI::reloadConfig();
+
+    g_iOffsetMultiplier = std::any_cast<Hyprlang::INT>(HyprlandAPI::getConfigValue(PHANDLE, "plugin:hyprcontexts:offset")->getValue());
+
+    Event::bus()->m_events.config.reloaded.listenStatic(onConfigReload);
 
     HyprlandAPI::addNotification(PHANDLE, "[HyprContexts] Initialized!", CHyprColor(0.0f, 1.0f, 0.0f, 1.0f), 5000);
 
